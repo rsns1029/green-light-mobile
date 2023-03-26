340c4a6bcf8cbc58458ba95d60902031114322ef
hmchung2 <hmchung2@gmail.com>
2023-03-26 22:02:15 +0900

    first setting

--- App.js
+++ App.js
@@ -1,20 +1,77 @@
-import { StatusBar } from "expo-status-bar";
-import { StyleSheet, Text, View } from "react-native";
+import React, { useCallback, useEffect, useState } from "react";
+import { Ionicons } from "@expo/vector-icons";
+import * as Font from "expo-font";
+import { Asset } from "expo-asset";
+import { StatusBar, View, Text } from "react-native";
+import { NavigationContainer } from "@react-navigation/native";
+// import LoggedOutNav from "./navigators/LoggedOutNav";
+import * as SplashScreen from "expo-splash-screen";
+import { ApolloProvider, useReactiveVar } from "@apollo/client";
+// import client, { isLoggedInVar, tokenVar, cache } from "./apollo";
+// import LoggedInNav from "./navigators/LoggedInNav";
+import AsyncStorage from "@react-native-async-storage/async-storage";
+import { AsyncStorageWrapper, persistCache } from "apollo3-cache-persist";
 
+SplashScreen.preventAutoHideAsync();
 export default function App() {
+  const [ready, setReady] = useState(false);
+  // const isLoggedIn = useReactiveVar(isLoggedInVar);
+
+  const startLoading = () => {
+    const fontsToLoad = [Ionicons.font];
+    const fontPromises = fontsToLoad.map((font) => Font.loadAsync(font));
+    const imagesToLoad = [
+      require("./assets/glLogo.png"),
+      "https://upload.wikimedia.org/wikipedia/commons/thumb/2/2a/Instagram_logo.svg/840px-Instagram_logo.svg.png",
+    ];
+    const imagePromises = imagesToLoad.map((image) => Asset.loadAsync(image));
+    return Promise.all([...fontPromises, ...imagePromises]);
+  };
+
+  const preload = async () => {
+    const token = await AsyncStorage.getItem("token");
+    if (token) {
+      console.log("token : ", token);
+      // isLoggedInVar(true);
+      // tokenVar(token);
+    }
+
+    return startLoading();
+  };
+
+  useEffect(() => {
+    async function prepare() {
+      try {
+        await preload();
+      } catch (e) {
+        console.warn(e);
+      } finally {
+        setReady(true);
+
+        /// temp !!!!  ///
+        // console.log("pk");
+        // isLoggedInVar(false);
+        // tokenVar("");
+        //////////////////
+      }
+    }
+    prepare();
+  }, []);
+
+  const onLayoutRootView = useCallback(async () => {
+    if (ready) {
+      await SplashScreen.hideAsync();
+    }
+  }, [ready]);
+
+  if (!ready) {
+    return null;
+  }
+
   return (
-    <View style={styles.container}>
+    <View style={{ flex: 1 }} onLayout={onLayoutRootView}>
+      <StatusBar hidden={false} />
       <Text>Open up App.js to start working on your app!!!!!!!!</Text>
-      <StatusBar style="auto" />
     </View>
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: "#fff",
-    alignItems: "center",
-    justifyContent: "center",
-  },
-});

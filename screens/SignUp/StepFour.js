955962dbaf6d6119544a5333a8a49183dee07689
hmchung2 <hmchung2@gmail.com>
2023-04-11 00:58:19 +0900

    commit

--- screens/SignUp/StepFour.js
+++ screens/SignUp/StepFour.js
@@ -1,17 +1,55 @@
-import React from "react";
-import { Text, View } from "react-native";
+import React, { useState } from "react";
+import { View, TouchableOpacity, Image } from "react-native";
+import { Ionicons } from "@expo/vector-icons";
+import * as ImagePicker from "expo-image-picker";
 
 export default function Notifications() {
+  const [avatar, setAvatar] = useState(null);
+
+  const handleAvatarPress = async () => {
+    // const { status } = await ImagePicker.requestCameraRollPermissionsAsync();
+
+    // if (status !== "granted") {
+    //   alert("Sorry, we need camera roll permissions to make this work!");
+    //   return;
+    // }
+
+    const result = await ImagePicker.launchImageLibraryAsync({
+      mediaTypes: ImagePicker.MediaTypeOptions.Images,
+      allowsEditing: true,
+      aspect: [1, 1],
+      quality: 1,
+    });
+
+    if (!result.canceled) {
+      setAvatar(result.assets[0].uri);
+    }
+  };
+
   return (
-    <View
-      style={{
-        backgroundColor: "black",
-        flex: 1,
-        alignItems: "center",
-        justifyContent: "center",
-      }}
-    >
-      <Text style={{ color: "white" }}>Notifications</Text>
+    <View style={{ flex: 1, alignItems: "center", justifyContent: "center" }}>
+      <TouchableOpacity onPress={handleAvatarPress}>
+        <View
+          style={{
+            height: 200,
+            width: 200,
+            borderRadius: 175,
+            backgroundColor: "gray",
+            alignItems: "center",
+            justifyContent: "center",
+            overflow: "hidden",
+          }}
+        >
+          {avatar ? (
+            <Image
+              source={{ uri: avatar }}
+              style={{ height: 200, width: 200 }}
+            />
+          ) : (
+            <Ionicons name="person" size={80} color="white" />
+          )}
+        </View>
+      </TouchableOpacity>
     </View>
   );
 }
